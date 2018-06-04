289d3e80e53b83e66ebe93c8b9ffe8a70f6893d0
zyhSDU <1114130407@qq.com>
2018-06-04 20:34:25 +0800

    0.Android 学习笔记之AndBase框架学习(五) 数据库ORM..注解，数据库对象映射...

---
+++ app/src/main/java/com/example/administrator/learnandbase/ac/BaseAbAc.kt
@@ -0,0 +1,35 @@
+package com.example.administrator.learnandbase.ac
+
+/**
+ * Created by Administrator on 2018/3/23 0023.
+ */
+import android.os.Bundle
+import android.view.View
+import com.ab.activity.AbActivity
+import com.example.administrator.learnandbase.R
+import com.example.administrator.learnandbase.interfaces.UiOperation
+import com.example.administrator.learnandbase.util.Utils
+
+abstract class BaseAbAc : AbActivity(), UiOperation {
+    protected val self: BaseAbAc
+        get() = this
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+//        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        super.onCreate(savedInstanceState)
+        setAbContentView(layoutResId)
+        initPermission()
+        initData()
+        initView()
+        Utils.setButtonOnClickListener(findViewById<View>(android.R.id.content), this)//Activity的根View
+        initListener()
+    }
+
+    open fun initPermission() {}
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_back -> finish()
+            else -> onClick(v, v.id)
+        }
+    }
+}
